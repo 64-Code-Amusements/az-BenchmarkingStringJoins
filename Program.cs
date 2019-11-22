5386292488c345816b128c50a659810f6e20ef7b
walt <2963975+waltiam@users.noreply.github.com>
2019-11-22 15:03:47 -0500

    the code that shows this

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkingStringBuilding
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<StringCat>();
+        }
+    }
+
+    public class StringCat
+    {
+        private readonly string theString = "wa3aw";
+        private readonly IEnumerable<string> theStrings;
+
+        public StringCat()
+        {
+            theStrings = Enumerable.Range(0, 99).Select(x => theString);
+        }
+
+        [Benchmark]
+        public string Join()
+        {
+            return string.Join(',', theStrings);
+        }
+
+        [Benchmark]
+        public string Builder()
+        {
+            var sb = new StringBuilder();
+            foreach (var rec in theStrings) sb.Append($"'{rec}',");
+            return sb.ToString().Substring(0, sb.Length - 1);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # BenchmarkingStringJoins
+
+**TL;DR** Use `String.Join` rather then a string building to concatentate collections of string.
+
+|  Method |     Mean |     Error |    StdDev |
+|-------- |---------:|----------:|----------:|
+|    Join | 2.742 us | 0.0186 us | 0.0174 us |
+| Builder | 4.161 us | 0.0366 us | 0.0324 us |
